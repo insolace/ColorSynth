0fcb800b5b9f3b4661369eb1a251747d5a9e9f9c
Eric Bateman <insolace@gmail.com>
2020-03-11 22:33:15 -0700

    Add graphics

---
+++ ASM/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ASM.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ASM.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=asm.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/asm.tar
